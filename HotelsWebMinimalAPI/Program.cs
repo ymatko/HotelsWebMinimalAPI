518e550efa5dd1b9640ec79b159c99eaa2300bf0
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-19 17:04:45 +0100

    Add endpoints (get, post, delete)

--- HotelsWebMinimalAPI/Program.cs
+++ HotelsWebMinimalAPI/Program.cs
@@ -16,6 +16,37 @@ var app = builder.Build();
 
 //app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+var hotels = new List<Hotel>();
+
+
+
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels", (Hotel hotel) =>
+{
+    var index = hotels.FindIndex(h => h.Id == hotel.Id);
+    if (index < 0)
+        throw new Exception("Not found");
+    hotels[index] = hotel;
+});
+app.MapDelete("/hotels/{id}", (int id) =>
+{
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0)
+        throw new Exception("Not found");
+    hotels.RemoveAt(index);
+});
+
+
 
 app.Run();
+
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
