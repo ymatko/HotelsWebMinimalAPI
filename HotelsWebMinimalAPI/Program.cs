6958840a2753c6ee1ad8de7a368137d43e6e072e
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-22 09:22:55 +0100

    Add annotation

--- HotelsWebMinimalAPI/Program.cs
+++ HotelsWebMinimalAPI/Program.cs
@@ -28,27 +28,42 @@ if (app.Environment.IsDevelopment())
 
 
 app.MapGet("/hotels", async (IHotelRepository repository) =>
-    Results.Ok(await repository.GetHotelsAsync()));
+    Results.Ok(await repository.GetHotelsAsync()))
+    .Produces<List<Hotel>>(StatusCodes.Status200OK)
+    .WithName("GetAllHotels")
+    .WithTags("Getters");
 app.MapGet("/hotels/{id}", async (IHotelRepository repository, int id) => 
-    await repository.GetHotelAsync(id) is Hotel hotel? Results.Ok(hotel): Results.NotFound());
+    await repository.GetHotelAsync(id) is Hotel hotel? Results.Ok(hotel): Results.NotFound())
+    .Produces<Hotel>(StatusCodes.Status200OK)
+    .WithName("GetHotel")
+    .WithTags("Getters"); ;
 app.MapPost("/hotels", async ([FromServices] IHotelRepository repository, [FromBody] Hotel hotel, HttpResponse response) =>
     {
         await repository.InsertHotelAsync(hotel);
         await repository.SaveAsync();
         return Results.Created($"/hotels/{hotel.Id}", hotel);
-    });
+    })
+    .Accepts<Hotel>("application/json")
+    .Produces<Hotel>(StatusCodes.Status201Created)
+    .WithName("CreateHotel")
+    .WithTags("Creators");
 app.MapPut("/hotels", async([FromServices] IHotelRepository repository, [FromBody] Hotel hotel) =>
-{
-    await repository.UpdateHotelAsync(hotel);
-    await repository.SaveAsync();
-    return Results.NoContent();
-});
+    {
+        await repository.UpdateHotelAsync(hotel);
+        await repository.SaveAsync();
+        return Results.NoContent();
+    })
+    .Accepts<Hotel>("application/json")
+    .WithName("UpdateHotel")
+    .WithTags("Updaters"); ;
 app.MapDelete("/hotels/{id}", async (IHotelRepository repository, int id) =>
-{
+    {
     await repository.DeleteHotelAsync(id);
     await repository.SaveAsync();
     return Results.NoContent();
-});
+    })
+    .WithName("DeleteHotel")
+    .WithTags("Deleters");
 
 app.UseHttpsRedirection();
 
