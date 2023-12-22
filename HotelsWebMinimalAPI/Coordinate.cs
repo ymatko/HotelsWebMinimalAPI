75ac4a439cce9bcb97ba31a6203cae41770775b1
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-22 11:56:10 +0100

    Add Coordinate

---
+++ HotelsWebMinimalAPI/Coordinate.cs
@@ -0,0 +1,17 @@
+﻿namespace HotelsWebMinimalAPI
+{
+    public record Coordinate(double Latitude, double Longitude)
+    {
+        public static bool TryParse(string input, out Coordinate? coordinate)
+        {
+            coordinate = default;
+            var splitArray = input.Split(',', 2);
+            if (splitArray.Length != 2) return false;
+            if (!double.TryParse(splitArray[0], out var lat)) return false;
+            if (!double.TryParse(splitArray[1], out var lon)) return false;
+            coordinate = new(lat, lon);
+            return false;
+
+        }
+    }
+}
